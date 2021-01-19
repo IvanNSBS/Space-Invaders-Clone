c5f0d8fee2a6e85759227a508a37c505eae5767b
Ivan Neves <insbs@cin.ufpe.br>
2021-01-18 22:28:26 -0300

    Adding Invader Spawner
    Invader Spawner had Spawn Animation method for juice

--- Assets/Scripts/Invaders/InvaderSpawner.cs
+++ Assets/Scripts/Invaders/InvaderSpawner.cs
@@ -1,25 +1,97 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
+using DG.Tweening;
 
 namespace Invaders
 {
+    #pragma warning disable CS0649
     public class InvaderSpawner : MonoBehaviour
     {
         #region Inspector Fields
+        [Header("Spawn Animation")] 
+        [SerializeField] private float m_spawnMoveDuration = 1.5f;
+        [SerializeField] private Vector2 m_spawnDelayRange = new Vector2(0.1f, 0.5f);
+        [SerializeField] private float m_overShoot = 0.5f;
+        
+        [Header("Invaders")]
+        [SerializeField] private int m_numberOfInvadersInRow = 11;
+        [SerializeField] private List<GameObject> m_rows;
+        [SerializeField] private float m_invaderCellSize = 5f;
+
+        [Header("Special Invader")] 
+        [SerializeField] private GameObject m_specialInvaderPrefab;
+        [SerializeField] private float m_specialInvaderSpawnDelay;
         #endregion Inspector Fields
         
         
-        // Start is called before the first frame update
-        void Start()
+        #region MonoBehaviour Methods
+        private void Awake()
         {
+            SpawnInvaders();
+        }
+        #endregion MonoBehaviour Methods
+        
+        
+        #region Methods
+
+        private void SpawnInvaders()
+        {
+            int rowCount = m_rows.Count;
+            float halfCellSize = m_invaderCellSize / 2f;
+            float yOffset = rowCount / 2f * m_invaderCellSize + halfCellSize;
+            float xOffset = -(m_numberOfInvadersInRow / 2f) * m_invaderCellSize + halfCellSize;
             
+            float currentYOffset = 0;
+            float currentXOffset = xOffset;
+            float outsideScreenY = GetOutsideScreenYPos();
+            
+            foreach (var invaderPrefab in m_rows)
+            {
+                for (int i = 0; i < m_numberOfInvadersInRow; i++)
+                {
+                    if(invaderPrefab == null)
+                        continue;
+                    
+                    var invader = Instantiate(invaderPrefab, transform);
+                    Vector3 spawnPosition = transform.position + new Vector3(currentXOffset, outsideScreenY, 0);
+                    invader.transform.position = spawnPosition;
+                    
+                    AnimateInvaderSpawn(invader.transform, transform.position.y + currentYOffset);
+                    currentXOffset += m_invaderCellSize;
+                }
+
+                currentXOffset = xOffset;
+                currentYOffset += m_invaderCellSize;
+            }
+        }
+        
+        private float GetOutsideScreenYPos()
+        {
+            Vector3 outsidePosNormalized = new Vector3(0f, 1.001F, 0f);
+            Vector3 worldPos = Camera.main.ViewportToWorldPoint(outsidePosNormalized);
+            return worldPos.y;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void AnimateInvaderSpawn(Transform invaderTransform, float targetYPosition)
         {
-            
+            Sequence sequence = DOTween.Sequence();
+            sequence.AppendInterval(Random.Range(m_spawnDelayRange.x, m_spawnDelayRange.y));
+            sequence.Append(invaderTransform.DOMoveY(targetYPosition, m_spawnMoveDuration).SetEase(Ease.OutBack, m_overShoot));
         }
+        #endregion Methods
+        
+        
+        #region Utility Methods
+        [ContextMenu("Respawn Invaders")]
+        private void RespawnInvaders()
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+            
+            SpawnInvaders();
+        }
+        #endregion Utility Methods
     }
 }
